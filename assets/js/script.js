ef10c4f7cf5f1ef678079ec768abf6ccc45263c0
ZoeMacf <zoemacfarlane93@gmail.com>
2023-09-18 22:50:16 +0100

    Update pattern function to create random array of colour shapes from div

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,13 @@
  * Creates a random pattern consisting of numbers 1-6 and adds them to pattern array.
  */
 function createPattern(){
-    let pattern = [1,2,3,4,5,6];
+    let shape1 = document.getElementById('shape1');
+    let shape2 = document.getElementById('shape2');
+    let shape3 = document.getElementById('shape3');
+    let shape4 = document.getElementById('shape4');
+    let shape5 = document.getElementById('shape5');
+    let shape6 = document.getElementById('shape6');
+    let pattern = [shape1,shape2,shape3,shape4,shape5,shape6];
     //To create a random shuffle of the array, the Fisher Yates shuffle was used after researching the sort() method.
     for (let i = pattern.length -1; i > 0; i--) {
         let a = Math.floor(Math.random() * (i+1));
@@ -11,7 +17,6 @@ function createPattern(){
         pattern[i] = pattern[a]
         pattern[a] = b;
     }
-    document.getElementById("test").innerHTML = pattern;
 }
 
 function displayPattern(){
