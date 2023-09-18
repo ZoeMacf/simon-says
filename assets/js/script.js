3390dd5007a1623b5b8f7f29d03ad1dac3cbacc3
ZoeMacf <zoemacfarlane93@gmail.com>
2023-09-18 22:39:40 +0100

    create function shuffle array and create a pattern for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
-function createPattern(){
 
+/**
+ * Creates a random pattern consisting of numbers 1-6 and adds them to pattern array.
+ */
+function createPattern(){
+    let pattern = [1,2,3,4,5,6];
+    //To create a random shuffle of the array, the Fisher Yates shuffle was used after researching the sort() method.
+    for (let i = pattern.length -1; i > 0; i--) {
+        let a = Math.floor(Math.random() * (i+1));
+        let b = pattern[i];
+        pattern[i] = pattern[a]
+        pattern[a] = b;
+    }
+    document.getElementById("test").innerHTML = pattern;
 }
 
 function displayPattern(){
